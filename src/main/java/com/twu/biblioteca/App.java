f88d0c6996a0557321d71a82eec2f0df0ad3b69b
Reynau <xavi.lopez.reynau@gmail.com>
2020-01-22 17:29:46 +0100

    [xavi][1.2] Print the list of books after the welcome message

--- src/main/java/com/twu/biblioteca/App.java
+++ src/main/java/com/twu/biblioteca/App.java
@@ -3,12 +3,16 @@
  */
 package com.twu.biblioteca;
 
+import java.util.Arrays;
+
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        WelcomePrinter welcomePrinter = new WelcomePrinter(System.out);
+        BookLibrary bookLibrary = new BookLibrary(Arrays.asList("Book1", "Book2", "Book3"));
+        BookLibraryPrinter bookLibraryPrinter = new BookLibraryPrinter(System.out, bookLibrary);
+
+        welcomePrinter.printWelcome();
+        bookLibraryPrinter.printBooks();
     }
 }
