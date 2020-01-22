80ecbcf7560f9d44242a96ccdc474d468b9e9458
Reynau <xavi.lopez.reynau@gmail.com>
2020-01-22 16:43:12 +0100

    [xavi][1.1] Fix WELCOME_MESSAGE typo on WelcomePrinter class

--- src/main/java/com/twu/biblioteca/WelcomePrinter.java
+++ src/main/java/com/twu/biblioteca/WelcomePrinter.java
@@ -5,13 +5,13 @@ import java.io.PrintStream;
 public class WelcomePrinter {
     private PrintStream printStream;
 
-    static final String WELCOMNE_MESSAGE = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    static final String WELCOME_MESSAGE = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
 
     public WelcomePrinter (PrintStream printStream) {
         this.printStream = printStream;
     }
 
     public void printWelcome () {
-        printStream.println(WELCOMNE_MESSAGE);
+        printStream.println(WELCOME_MESSAGE);
     }
 }
