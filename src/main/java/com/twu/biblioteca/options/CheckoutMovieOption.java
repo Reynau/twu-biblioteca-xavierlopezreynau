1677780abd27a44293386a6c17eaf588c23758d4
Reynau <xavi.lopez.reynau@gmail.com>
2020-01-27 11:22:48 +0100

    [xavi][2.2] Add CheckoutMovieOption option

---
+++ src/main/java/com/twu/biblioteca/options/CheckoutMovieOption.java
@@ -0,0 +1,56 @@
+package com.twu.biblioteca.options;
+
+import com.twu.biblioteca.*;
+import com.twu.biblioteca.exceptions.InvalidBook;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class CheckoutMovieOption extends Option {
+    private static final String TITLE = "Check-out movie";
+
+    BufferedReader reader;
+    Printer printer;
+    Library<Movie> movieLibrary;
+
+    public CheckoutMovieOption(Printer printer, BufferedReader reader, Library<Movie> movieLibrary) {
+        super(TITLE);
+
+        this.reader = reader;
+        this.printer = printer;
+        this.movieLibrary = movieLibrary;
+    }
+
+    @Override
+    public void execute() {
+        int optionNumber = requestMovieNumber();
+
+        try {
+            movieLibrary.checkoutItem(optionNumber);
+        }
+        catch (InvalidBook e) {
+            printer.print(e.getMessage());
+        }
+    }
+
+    private int requestMovieNumber() {
+        String input = null;
+        int optionNumber;
+
+        try {
+            input = reader.readLine();
+        }
+        catch (IOException e) {
+            printer.print("Error reading input. Please try again.");
+        }
+
+        try {
+            optionNumber = Integer.parseInt(input);
+        }
+        catch (NumberFormatException e) {
+            printer.print("Invalid option. Please try again.");
+            return -1;
+        }
+        return optionNumber;
+    }
+}
---
+++ src/test/java/com/twu/biblioteca/options/CheckoutMovieOptionTest.java
@@ -0,0 +1,39 @@
+package com.twu.biblioteca.options;
+
+import com.twu.biblioteca.Library;
+import com.twu.biblioteca.Movie;
+import com.twu.biblioteca.Printer;
+import com.twu.biblioteca.exceptions.InvalidBook;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
+public class CheckoutMovieOptionTest {
+    Printer printer;
+    BufferedReader reader;
+    Library<Movie> movieLibrary;
+    CheckoutMovieOption checkoutMovieOption;
+
+    @Before
+    public void setUp () {
+        printer = mock(Printer.class);
+        reader = mock(BufferedReader.class);
+        movieLibrary = mock(Library.class);
+        checkoutMovieOption = new CheckoutMovieOption(printer, reader, movieLibrary);
+    }
+
+    @Test
+    public void shouldCheckoutSelectedMovie () throws IOException, InvalidBook {
+        int bookNumber = 1;
+        when(reader.readLine()).thenReturn(Integer.toString(bookNumber));
+
+        checkoutMovieOption.execute();
+
+        verify(movieLibrary).checkoutItem(bookNumber);
+        verifyNoMoreInteractions(movieLibrary);
+    }
+}
