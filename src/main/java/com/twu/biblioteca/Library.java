6a3713738e4417008244c80f01774aaa411441d5
Reynau <xavi.lopez.reynau@gmail.com>
2020-01-27 10:00:15 +0100

    [xavi][2.1] Add Library class

---
+++ src/main/java/com/twu/biblioteca/Library.java
@@ -0,0 +1,69 @@
+package com.twu.biblioteca;
+
+import com.twu.biblioteca.exceptions.InvalidBook;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Library<T extends Printable> implements Printable {
+    Printer printer;
+
+    private List<T> items;
+    private List<Boolean> checkedOut;
+
+    public Library(Printer printer, List<T> items) {
+        this.printer = printer;
+
+        this.items = items;
+        this.checkedOut = Arrays.asList(new Boolean[items.size()]);
+        Collections.fill(checkedOut, Boolean.FALSE);
+    }
+
+     public void checkoutItem (int itemNumber) throws InvalidBook {
+         checkIfIsOutOfBounds(itemNumber);
+
+         int itemIndex = itemNumber-1;
+
+         if (this.checkedOut.get(itemIndex)) {
+             printer.print("Sorry, that item is not available");
+             return;
+         }
+
+         this.checkedOut.set(itemIndex, Boolean.TRUE);
+         printer.print("Thank you! Enjoy the item");
+     }
+
+     public void returnItem (int itemNumber) throws InvalidBook {
+         checkIfIsOutOfBounds(itemNumber);
+
+         int itemIndex = itemNumber-1;
+
+         if (!this.checkedOut.get(itemIndex)) {
+             printer.print("That is not a valid item to return");
+             return;
+         }
+
+         this.checkedOut.set(itemIndex, Boolean.FALSE);
+         printer.print("Thank you for returning the item");
+     }
+
+    @Override
+    public String serialize() {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < items.size(); ++i){
+            if (checkedOut.get(i)) continue;
+
+            T item = items.get(i);
+            result.append(i+1).append(". ").append(item.serialize()).append("\n");
+        }
+        return result.toString();
+    }
+
+    private void checkIfIsOutOfBounds(int itemNumber) throws InvalidBook {
+        if (itemNumber < 1 || itemNumber > this.checkedOut.size()) {
+            throw new InvalidBook("Invalid item number");
+        }
+    }
+}
---
+++ src/test/java/com/twu/biblioteca/LibraryTest.java
@@ -0,0 +1,101 @@
+package com.twu.biblioteca;
+
+import com.twu.biblioteca.exceptions.InvalidBook;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+
+public class LibraryTest {
+    private Printer printer;
+    private List<Book> books;
+
+    Library<Book> bookLibrary;
+
+    @Before
+    public void setUp () {
+        printer = mock(Printer.class);
+
+        books = new ArrayList<>();
+        for (int i=1; i < 4; ++i) {
+            Book book = mock(Book.class);
+            when(book.serialize()).thenReturn("Book"+i+":Author"+i+":Year"+i);
+            books.add(book);
+        }
+
+        bookLibrary = new Library<>(printer, books);
+    }
+
+    @Test
+    public void shouldReturnSerializedBookLibrary () {
+        String expected = "1. Book1:Author1:Year1\n2. Book2:Author2:Year2\n3. Book3:Author3:Year3\n";
+        String actual = bookLibrary.serialize();
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldBePrintable () {
+        Class<?> classObj = BookLibrary.class;
+
+        assertThat(Printable.class.isAssignableFrom(classObj), is(true)); // checks if implements printable
+    }
+
+    @Test
+    public void shouldStoreCheckedOutStateOfBooks () throws InvalidBook {
+        bookLibrary.checkoutItem(1);
+
+        String actual = bookLibrary.serialize();
+        String expected = "2. Book2:Author2:Year2\n3. Book3:Author3:Year3\n";
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldPrintSuccessMessageWhenBookIsCheckedOut () throws InvalidBook {
+        bookLibrary.checkoutItem(3);
+
+        verify(printer).print("Thank you! Enjoy the item");
+        verifyNoMoreInteractions(printer);
+    }
+
+    @Test
+    public void shouldPrintUnSuccessfulMessageWhenBookIsCheckedOut () throws InvalidBook {
+        bookLibrary.checkoutItem(3);
+        bookLibrary.checkoutItem(3);
+
+        verify(printer).print("Sorry, that item is not available");
+    }
+
+    @Test
+    public void shouldStoreReturnedBookState () throws InvalidBook {
+        bookLibrary.checkoutItem(1);
+        bookLibrary.returnItem(1);
+
+        String actual = bookLibrary.serialize();
+        String expected = "1. Book1:Author1:Year1\n2. Book2:Author2:Year2\n3. Book3:Author3:Year3\n";
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void shouldPrintSuccessfulMessageWhenBookIsReturned () throws InvalidBook {
+        bookLibrary.checkoutItem(3);
+        bookLibrary.returnItem(3);
+
+        verify(printer).print("Thank you for returning the item");
+    }
+
+    @Test
+    public void shouldPrintUnSuccessfulMessageWhenBookIsReturned () throws InvalidBook {
+        bookLibrary.returnItem(3);
+
+        verify(printer).print("That is not a valid item to return");
+    }
+}
