13fc909d7e4e80ddd5330b7f63ac9612555651c5
Reynau <xavi.lopez.reynau@gmail.com>
2020-01-28 11:06:56 +0100

    [xavi][2.4] Add missing tests for ViewCheckedOut(Books/Movies)

---
+++ src/test/java/com/twu/biblioteca/options/ViewCheckedOutBooksOptionTest.java
@@ -0,0 +1,29 @@
+package com.twu.biblioteca.options;
+
+import com.twu.biblioteca.Book;
+import com.twu.biblioteca.Library;
+import com.twu.biblioteca.Printer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class ViewCheckedOutBooksOptionTest {
+    Printer printer;
+    Library<Book> bookLibrary;
+    ViewCheckedOutBooksOption viewCheckedOutBooksOption;
+
+    @Before
+    public void setUp() throws Exception {
+        printer = mock(Printer.class);
+        bookLibrary = mock(Library.class);
+        viewCheckedOutBooksOption = new ViewCheckedOutBooksOption(printer, bookLibrary);
+    }
+
+    @Test
+    public void shouldPrintTheListOfCheckedOutBooks () {
+        viewCheckedOutBooksOption.execute();
+        verify(printer).print(bookLibrary.getCheckedOutBooks());
+        verifyNoMoreInteractions(printer);
+    }
+}
---
+++ src/test/java/com/twu/biblioteca/options/ViewCheckedOutMoviesOptionTest.java
@@ -0,0 +1,29 @@
+package com.twu.biblioteca.options;
+
+import com.twu.biblioteca.Library;
+import com.twu.biblioteca.Movie;
+import com.twu.biblioteca.Printer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class ViewCheckedOutMoviesOptionTest {
+    Printer printer;
+    Library<Movie> movieLibrary;
+    ViewCheckedOutMoviesOption viewCheckedOutMoviesOption;
+
+    @Before
+    public void setUp() throws Exception {
+        printer = mock(Printer.class);
+        movieLibrary = mock(Library.class);
+        viewCheckedOutMoviesOption = new ViewCheckedOutMoviesOption(printer, movieLibrary);
+    }
+
+    @Test
+    public void shouldPrintTheListOfCheckedOutBooks () {
+        viewCheckedOutMoviesOption.execute();
+        verify(printer).print(movieLibrary.getCheckedOutBooks());
+        verifyNoMoreInteractions(printer);
+    }
+}
